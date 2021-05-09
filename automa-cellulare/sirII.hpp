e720d41a63cd76044c4d369104a062517743be91
Giovanni Raumer <giovanni.raumer@studio.unibo.it>
2021-05-09 17:21:40 +0200

    .gitignore aggiornato e automa cellulare in lavorazione

---
+++ automa-cellulare/sirII.hpp
@@ -0,0 +1,83 @@
+#ifndef SIRII_HPP
+#define SIRII_HPP
+
+#include <iostream>
+#include <vector>
+
+namespace pandemic {
+
+enum class Human: char {Susceptible,Infectd,Recovered};
+
+class Population {
+  using Row = std::vector<Human>;
+  using Grid = std::vector<Row>;
+  int m_side;
+  Grid m_grid;
+
+  friend bool operator==(Population const& left, Population const& right) {  // ripassa il motivo di friend per l'esame
+    return left.m_grid == right.m_grid;
+  }
+  friend bool operator!=(Population const& left, Population const& right) {
+    return left.m_grid != right.m_grid;
+  }
+
+ public:
+  Population(int N)
+      : m_side(N),m_grid(N, Row(N, Human::Susceptible)) {  //vector con tonde:inizializza un vettore lungo N con gli elementi dopo la virgola
+  }  
+  Human const& human(int r, int c) const { 
+    return m_grid[r][c]; 
+    }
+  Human& human(int r, int c) { 
+    return m_grid[r][c]; 
+    }
+  int side() const { return m_side; }
+};
+
+inline int alive_neighbours(Population const& population, int x, int y) {
+  int counter = 0;
+  for (int i : {-1, 0, 1}) {
+    for (int j : {-1, 0, 1}) {
+      if (i == 0 && j == 0) {
+        continue;
+      }
+      if (x+i<=0 || (x+i>=population.side()) || y+j<=0 || y+j>=population.side()) {
+        continue;
+      }
+      if (population.human(x + i, y + j) == Human::Alive) {
+        ++counter;
+      }
+    }
+  }
+  return counter;
+}
+
+inline Population evolve(Population const& current) {
+  int const N = current.side();
+  Population next(N);
+  for (int r = 0; r != N; ++r) {
+    for (int c = 0; c != N; ++c) {
+      int const m=alive_neighbours(current, r, c);
+      if (current.human(r,c)==Human::Dead && m==3) {
+        next.human(r, c) = Human::Alive;
+      } 
+      else if (current.human(r,c)==Human::Alive && m==2) {
+        next.human(r, c) = Human::Alive;
+      }  
+      else if (current.human(r,c)==Human::Alive && m==3) {
+        next.human(r, c) = Human::Alive;
+      }
+      else {
+        next.human(r, c) = Human::Dead;
+      }  
+    }
+  }
+  return next;
+}
+
+bool operator==(Population const& left, Population const& right);
+bool operator!=(Population const& left, Population const& right);
+
+}  // namespace pandemic
+
+#endif
