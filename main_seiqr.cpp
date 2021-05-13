251c1c2a5f081dd10e77da3ec99c82475589d7dc
Samo <samo.severino@studio.unibo.it>
2021-05-13 13:47:57 +0200

    update seiqr (incompleto)

--- main_seiqr.cpp
+++ main_seiqr.cpp
@@ -12,7 +12,7 @@ void print(std::vector<State> const& states)
                "+-----------+-----------+-----------+-----------+-----------+\n"
             << std::fixed;
   for (auto& state : states) {
-    std::cout << "| " << std::setw(9) << state.S << " | " << std::setw(9) << state.E << " | "<< std::setw(9) << state.I << std::setw(9) << state.Q << std::setw(9) << state.R <<" |\n";
+    std::cout << "| " << std::setw(9) << state.S << " | " << std::setw(9) << state.E << " | "<< std::setw(9) << state.I << " | " << std::setw(9) << state.Q << " | " << std::setw(9) << state.R <<" |\n";
   }
   std::cout << "+-----------+-----------+-----------+-----------+-----------+\n";
 }
@@ -22,8 +22,8 @@ void print(std::vector<State> const& states)
 int main(int argc, char* argv[]) {
 try {
 
-if (argc!=7) {
-  throw std::runtime_error{"6 inputs required"};
+if (argc!=12) {
+  throw std::runtime_error{"11 inputs required"};
 }
 
 for (int i=1; i!=4;++i) {
@@ -32,24 +32,29 @@ for (int i=1; i!=4;++i) {
   } 
 }
 
-if (std::stod(argv[5])>1 || std::stod(argv[6])>1 || std::stod(argv[5])<=0. || std::stod(argv[6])<=0.) {
-  throw std::runtime_error{"gamma and beta must be positive and less than 1"};
-}
+/*for (int j=5; j!=11;++j) {
+  if (std::stod(argv[j]) <= 0. || std::stod(argv[j]) > 1) {
+    throw std::runtime_error{"all coefficients must be positive and less than 1"};
+  } 
+}*/
 
 int S0=std::atoi(argv[1]);
 int E0=std::atoi(argv[2]);
 int I0=std::atoi(argv[3]);
 int T=std::atoi(argv[4]);
-double beta=std::stod(argv[5]);
-double gamma=std::stod(argv[6]);
+double alfa=std::stod(argv[5]);
+double beta=std::stod(argv[6]);
+double gamma=std::stod(argv[7]);
+double d=std::stod(argv[8]);
+double k=std::stod(argv[9]);
+double delta=std::stod(argv[10]);
+double epsilon=std::stod(argv[11]);
 
 State s0{};
 s0.S=S0;
 s0.E=E0;
-s0.I =I0;
-s0.R =0;
-s0.b =beta;
-s0.g =gamma;
+s0.I=I0; 
+s0.R=0;
 Epidemic covid{s0};
 auto const states=covid.evolve(T);
 print(states);
--- seiqr.hpp
+++ seiqr.hpp
@@ -12,10 +12,6 @@ struct State {
   int I;
   int Q;
   int R;
-  double a; //protection rate
-  double b; //infection rate
-  double g; //recovery rate
-  double d; //inverse of quarantine time
 };
 
 class Epidemic {
@@ -27,18 +23,25 @@ class Epidemic {
   std::vector<State> evolve(int T) {
     std::vector<State> result{e_initial_state};
     int const N = result.back().S + result.back().E + result.back().I + result.back().Q + result.back().R;
+    double a; //E-->I
+    double b; //infection rate
+    double g; //I-->R
+    double d; //natural mortality rate
+    double k; //E-->R
+    double l; //I-->Q
+    double e; //Q-->R
     for (int t = 1; t != T; ++t) {
       State const& prev = result.back();
       State next;
-      next.S = round(prev.S - prev.b * prev.S * prev.I / N);
-      next.I = round(prev.I + prev.b * prev.S * prev.I / N - prev.g * prev.I);
-      next.R = N-next.S-next.I;
-      next.b = prev.b;
-      next.g = prev.g;
-      if (next.S==prev.S && next.I==prev.I && next.R==prev.R) {
+      next.S = round(prev.S - b * prev.S * (prev.I + prev.E) / N);
+      next.E = round(prev.E + b * prev.S * (prev.E + prev.I) / N -(d + a + k) * prev.E);
+      next.I = round(prev.I + a * prev.E - (d + g + l) * prev.I);
+      next.Q = round(prev.Q + l * prev.I - (d + e) * prev.Q);
+      next.R = N-next.S-next.E-next.I-next.Q;
+      /*if (next.S==prev.S && next.E==prev.E && next.I==prev.I && next.Q==prev.Q && next.R==prev.R) {
         next.I=prev.I-1;
         next.R=prev.R+1;
-      }
+      }*/
       assert(N==next.S+next.E+next.I+next.Q+next.R);
       result.push_back(next);
       if (next.I==0) {
