c5e3b18f53bf7df3b735c72ea7f4c43a0b24b21f
Giovanni Raumer <giovanni.raumer@studio.unibo.it>
2021-05-30 14:29:37 +0200

    aggiunta di q e v

--- automa-cellulare/main.cpp
+++ automa-cellulare/main.cpp
@@ -3,12 +3,12 @@
 #include <thread>
 #include <string> //per std::stod
 
-void print(pandemic::Population const &population, situation::State state)
+void print(pandemic::Population const &population)
 {
     const auto N = population.side();
 
     std::cout << "\033[2J";
-    std::cout << "| S : " << state.S << " | " << "I : " << state.I << " | " << "R : " << state.R << " | " <<'\n';
+    std::cout << "| S : " << N*N-population.infected()-population.recovered() << " | " << "I : " << population.infected() << " | " << "R : " << population.recovered() << " | " <<'\n';
     std::cout << '+' << std::string(N, '-') << "+\n";
     for (int r = 0; r != N; ++r)
     {
@@ -37,34 +37,57 @@ int main(int argc, char *argv[])
 {
     try
     {
-        if (argc != 6)
-        {
-            throw std::runtime_error{"5 inputs required"};
+    switch (argc) {
+      case (argc != 6 || argc != 9):
+        throw std::runtime_error{"5 or 8 inputs required"};
+        break;
+
+      case (argc == 6 || argc == 9):
+        for (int i = 1; i != 4; ++i) {
+          if (atoi(argv[i]) <= 0) {
+            throw std::runtime_error{"inputs must be positive and different from zero"};
+          }
         }
 
-        for (int i = 1; i != 4; ++i)
-        {
-            if (atoi(argv[i]) <= 0)
-            {
-                throw std::runtime_error{
-                    "inputs must be positive and different from zero"};
-            }
+        for (int i = 4; i != 6; ++i) {
+          if (std::stod(argv[i]) <= 0 || std::stod(argv[i]) > 1) {
+            throw std::runtime_error{
+                "gamma and beta must be positive and less than 1"};
+          }
         }
 
-        for (int i = 4; i != 6; ++i)
-        {
-            if (std::stod(argv[i]) <= 0 || std::stod(argv[i]) > 1)
-            {
-                throw std::runtime_error{
-                    "gamma and beta must be positive and less than 1"};
-            }
+        if (std::atoi(argv[2]) > std::atoi(argv[1]) * std::atoi(argv[1])) {
+          throw std::runtime_error{"infected must be less than/equal to total"};
+        }
+        break;
+
+        case(argc==9):
+        if (tolower(static_cast<char>(argv[6]))!="v" || tolower(static_cast<char>(argv[6]))!="q") {
+            throw std::runtime_error{"6th input must be \"v\" or \"q\""};
         }
 
-        if (std::atoi(argv[2]) > std::atoi(argv[1])*std::atoi(argv[1])){
-            throw std::runtime_error {
-                "infected must be less than/equal to total"
-            };
+        case (argc==9 && tolower(static_cast<char>(argv[6]))=="v"):
+        if(std::atoi(argv[7])>std::atoi(argv[3])) {
+            throw std::runtime_error{"the vaccination period should start before the last iteration"};
         } 
+        if(std::atoi(argv[8])<0 || std::atoi(argv[8])>1) {
+            throw std::runtime_error{"vaccine effectiveness must be positive and less than 1"};
+        }
+        int const v_begin=std::atoi(argv[7]);
+        double const v_eff=std::stod(argv[8]);
+        break;
+
+        case (argc==9 && tolower(static_cast<char>(argc[6]))=="q"):
+        if(std::atoi(argv[7])>std::atoi(argv[3])) {
+            throw std::runtime_error{"the quarantine period should start before the last iteration"};
+        }
+        if(std::atoi(argv[8])<0) {
+            throw std::runtime_error{"the quarantine period must be a positive number"};
+        }
+        int const q_begin=std::atoi(argv[7]);
+        int const q_period=std::atoi(argv[8]);
+        break
+    }
 
         int const N = std::atoi(argv[1]);
         int const I0 = std::atoi(argv[2]);
@@ -82,9 +105,9 @@ int main(int argc, char *argv[])
 
         for (int i = 0; i != T; ++i)
         {
-            print(population, state);
+            print(population);
             std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-            if (state.I == 0)
+            if (population.infected() == 0)
             {
                 break;
             }
