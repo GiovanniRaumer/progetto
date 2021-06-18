4f089664f44fa026f972e572220ed7025a4244df
Arnaldo Bartoli <arnaldo.bartoli@studio.unibo.it>
2021-06-18 18:57:46 +0200

    vaccinazioni quasi finite

--- automa-cellulare/evolve.hpp
+++ automa-cellulare/evolve.hpp
@@ -21,14 +21,25 @@ inline pandemic::Population evolve(pandemic::Population const &current,
 
   // VACCINATION
   if (v_ok == true) {
-    int vax_per_day = N * N * 0.001;
+    int vax_per_day = N * N * 0.01;
 
     while (vax_per_day > 0) {
+      if (vax_per_day >= N * N - current.infected() - current.recovered() - current.vaccinated()) {
+        for (int r = 0; r != N; ++r) {
+          for (int c = 0; c != N; ++c) {
+            if (current.human(r, c).Is == pandemic::Human::S) {
+                next.human(r, c).v = true;
+            }
+          }
+        }
+        break;
+      }
       std::default_random_engine eng{std::random_device{}()};
       std::uniform_int_distribution<int> pos{0, N};
       int r = pos(eng);
       int c = pos(eng);
-      if (current.human(r, c).Is == pandemic::Human::S && current.human(r, c).v == false) {
+      if (current.human(r, c).Is == pandemic::Human::S &&
+          current.human(r, c).v == false) {
         next.human(r, c).v = true;
         --vax_per_day;
       }
@@ -51,7 +62,14 @@ inline pandemic::Population evolve(pandemic::Population const &current,
             auto x = distr(eng);
             auto y = distr(eng);
             if (current.human(r + x, c + y).Is == pandemic::Human::S) {
-              next.human(r + x, c + y).Is = pandemic::Human::I;
+              if (current.human(r + x, c + y).v == true) {
+                auto vax_res = dist(eng);
+                if (vax_res > v_eff) {
+                  next.human(r + x, c + y).Is = pandemic::Human::I;
+                }
+              } else {
+                next.human(r + x, c + y).Is = pandemic::Human::I;
+              }
               ++i_left;
             } else {
               continue;
@@ -64,7 +82,14 @@ inline pandemic::Population evolve(pandemic::Population const &current,
             auto x = distr(eng);
             auto y = distr(eng);
             if (current.human(r + x, c + y).Is == pandemic::Human::S) {
-              next.human(r + x, c + y).Is = pandemic::Human::I;
+              if (current.human(r + x, c + y).v == true) {
+                auto vax_res = dist(eng);
+                if (vax_res > v_eff) {
+                  next.human(r + x, c + y).Is = pandemic::Human::I;
+                }
+              } else {
+                next.human(r + x, c + y).Is = pandemic::Human::I;
+              }
               ++i_left;
             } else {
               continue;
