e6a86cb84bed18173f20bc5c0c37c4346a61a5fd
Arnaldo Bartoli <arnaldo.bartoli@studio.unibo.it>
2021-06-23 16:05:09 +0200

    sirq test finiti

--- sirq_test.cpp
+++ sirq_test.cpp
@@ -1,7 +1,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "sirq.hpp"
-
 #include "doctest.h"
+
+#include "sirq.hpp"
 
 TEST_CASE("Testing equations") {
   State initial_state;
@@ -10,30 +10,35 @@ TEST_CASE("Testing equations") {
   initial_state.b = 0.7;
   initial_state.g = 0.4;
 
-  int T = 20;
-  int q_start = 1.;
-  int q_eff = 1.;
+  int const N = initial_state.S + initial_state.I + initial_state.R;
+
+  int T = 200;
 
   Epidemic ep(initial_state);
-  auto const evolution = ep.evolve(T, q_start, q_eff);
-  auto const last_state = evolution.back();
 
-  SUBCASE("Testing conservation of total population") {
-    CHECK(initial_state.S + initial_state.I + initial_state.R == last_state.S + last_state.I + last_state.R);
-  }
-
-  SUBCASE("Testing end of epidemic (Infected = 0 ?)") {
+  SUBCASE("Without quarantine") {
+    int q_start = 1.;
+    int q_eff = 1.;
+    auto const evolution = ep.evolve(T, q_start, q_eff);
+    auto const last_state = evolution.back();
+    // Testing conservation of total population
+    CHECK(last_state.S + last_state.I + last_state.R == N);
+    // Testing end of epidemic --> Infected = 0
     CHECK(last_state.I == 0);
+    // Testing conservation of beta
+    CHECK(initial_state.b == last_state.b);
   }
 
-  SUBCASE("Testing conservation of beta") {
-    CHECK(last_state.b == last_state.b);
-  }
-
-  q_start = 0.1;
-  q_eff = 0.6;
-
-  SUBCASE("Testing non-conservation of beta") {
-    CHECK(last_state.b != last_state.b);      
+  SUBCASE("With quarantine") {
+    int q_start = 0.1;
+    int q_eff = 0.6;
+    auto const evolution = ep.evolve(T, q_start, q_eff);
+    auto const last_state = evolution.back();
+    // Testing conservation of total population
+    CHECK(last_state.S + last_state.I + last_state.R == N);
+    // Testing end of epidemic --> Infected = 0
+    CHECK(last_state.I == 0);
+    // Testing non-conservation of beta
+    CHECK(initial_state.b > last_state.b);
   }
 }
